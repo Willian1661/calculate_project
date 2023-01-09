f0e9e21bee0e713fff5288e96249325c842283c8
willian1661 <willianderson.sn@gmail.com>
2023-01-09 11:57:47 -0300

    creating calculator-component

--- src/components/calculator-component.js
+++ src/components/calculator-component.js
@@ -1,3 +1,4 @@
+const button = require("../../src/components/button")
 function calculator(id) {
     return`
     <div id="${id}">
@@ -5,8 +6,26 @@ function calculator(id) {
             <input type="text" placeholder="0" id="input-screen-id">
         </div>
         <div id="buttons-dIv-id">
-            <button class="buttons-class">buttons-content</button>
-            <button class="buttons-class">buttons-content</button>
+        ${button("C","clear-all upper left")}
+        ${button("+/-","less-more upper")}
+        ${button("CE","del upper")}
+        ${button("%","upper right")}
+        ${button("7","left")}
+        ${button("8")}
+        ${button("9")}
+        ${button("/","right")}
+        ${button("4","left")}
+        ${button("5")}
+        ${button("6")}
+        ${button("*","right")}
+        ${button("1","left")}
+        ${button("2")}
+        ${button("3")}
+        ${button("+","right")}
+        ${button("0","bottom left")}
+        ${button(".","bottom")}
+        ${button("=","equal bottom")}
+        ${button("-","bottom right")}
         </div>
     </div>
     `;
--- test/components/calculator-component.test.js
+++ test/components/calculator-component.test.js
@@ -1,12 +1,7 @@
-// const button = require("../../src/components/button");
 const calculator = require("../../src/components/calculator-component")
 
 describe("calculator screen component", () =>{
     
-    // const buttons =[ 
-    //     button("foo","fooClass"),
-    //     button("baz","bazClass")
-    // ]
     it("is passing calculator screen", () => {
         expect(calculator("body-calc-id")).toMatchSnapshot()
     });
