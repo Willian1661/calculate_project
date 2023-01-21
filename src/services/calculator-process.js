d9b5e3b1c8ced7c71a254632bc06f7658d530de0
willian1661 <willianderson.sn@gmail.com>
2023-01-21 09:16:09 -0300

    button's content test

--- src/services/calculator-process.js
+++ src/services/calculator-process.js
@@ -1,12 +1,22 @@
-const visorInteractor  = require("./visor-interactor");
-
-function calculatorProcess(componentContent) {
-if (componentContent !== undefined) {
-       visorInteractor(componentContent)
-       return componentContent
-}
-return componentContent
-
+let memory = ""
+let memory2 = []
+function calculatorProcess(content,visor) {
+       if (content == "C") {
+              memory = ""
+              memory2 = []
+              return;
+       }
+       if (content == "=") {
+          if (memory2.includes("+")) {
+              let result = Number(memory2[0]) + Number(memory2[2])
+              
+              visor(String(result))
+          }
+       }
+       memory += content
+       memory2.push(content)
+       visor(memory)
+return console.log(memory2);
 }
 
 module.exports = calculatorProcess
--- test/components/calculator-process.test.js
+++ test/components/calculator-process.test.js
@@ -1,14 +1,24 @@
-jest.mock("../../src/services/visor-interactor")
 const calculatorProcess = require("../../src/services/calculator-process")
-const visorInteractor = require("../../src/services/visor-interactor")
 
 describe("button's content is passing", () => {
-  
-  it("has passed process's content into view", () => {
+  const operations = [
+    ["10", "+","30","=","40"],
+    ["10", "+","10","=","20"],
+  ];
 
-    expect(calculatorProcess("1")).toBe("1");
+	it.each(operations)("has a button content and closed tag", (oper1,oper2,oper3,oper4,oper5) => {
+		let visorMock = jest.fn()
+    calculatorProcess(oper1,visorMock)
+    calculatorProcess(oper2,visorMock)
+    calculatorProcess(oper3,visorMock)
+    calculatorProcess(oper4,visorMock)
+    calculatorProcess("C",visorMock)
 
-    expect(visorInteractor).toHaveBeenCalledTimes(1);
-  });
+    expect(visorMock.mock.calls[0][0]).toBe(oper1);
+    expect(visorMock.mock.calls[1][0]).toBe(oper1 + oper2);
+    expect(visorMock.mock.calls[2][0]).toBe(oper1 + oper2 + oper3);
+    expect(visorMock.mock.calls[3][0]).toBe(oper5);
+    console.log(oper5);
+	});
 });
 
